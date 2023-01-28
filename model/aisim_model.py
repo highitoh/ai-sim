eab72264335973712a7c1a649350214401f05d80
highitoh <highitoh@gmail.com>
2023-01-28 17:17:09 +0900

    Separate Cloud Charge Model to the New Model

--- model/aisim_model.py
+++ model/aisim_model.py
@@ -2,6 +2,8 @@ import random
 import numpy as np
 import aisim_model_param as p
 
+import charge_model
+
 EDGE = 0
 CLOUD = 1
 
@@ -258,29 +260,12 @@ class AISimModel:
     cloud_time_avg = cloud_time_all / self.ITERATION
     self.task_time_avg = task_time_all / self.ITERATION
     self.comm_time_avg = comm_time_all / self.ITERATION
-    self.charge = self.calc_charge(self.time_avg, self.data_avg, self.out_data_avg, cloud_access_num, cloud_time_avg)
+    self.charge = charge_model.calc_charge(self.time_avg, self.data_avg, self.out_data_avg, cloud_access_num, cloud_time_avg)
     print("time_avg=" + str(self.time_avg) + ", time_max=" + str(time_max) + ", time_min=" + str(time_min))
     print("data_avg=" + str(self.data_avg) + ", data_max=" + str(data_max) + ", data_min=" + str(data_min))
     print("charge=" + str(self.charge))
     print("task_time=" + str(self.task_time_avg))
     print("comm_time_avg=" + str(self.comm_time_avg))
-
-  def calc_charge(self, time_avg, data_avg, out_data_avg, cloud_access_num, cloud_time_avg):
-    charge = 0
-    if p.CLOUD_SERVICE == "EC2":
-      charged_out_data = max(p.REQUEST_NUM * out_data_avg - p.OUT_DATA_COST_FREE_TIER, 0)
-      charge += p.EC2_INSTANCE_NUM * p.EC2_COST_PER_HOUR * 730
-      charge += p.EBS_STORAGE_SIZE * p.EBS_COST_PER_GB * p.EC2_INSTANCE_NUM
-      charge += p.OUT_DATA_COST_PER_GB * (charged_out_data / 1000000000)
-    elif p.CLOUD_SERVICE == "LAMBDA":
-      exec_time_s = p.REQUEST_NUM * cloud_time_avg
-      charged_request_num = max(p.REQUEST_NUM - p.LAMBDA_REQ_FREE_TIER, 0)
-      charged_exec_time_s = max(exec_time_s - p.LAMBDA_GBS_FREE_TIER, 0)
-      charged_out_data    = max(p.REQUEST_NUM * out_data_avg - p.OUT_DATA_COST_FREE_TIER, 0)
-      charge += cloud_access_num * p.LAMBDA_MEM_SIZE * charged_exec_time_s * p.LAMBDA_COST_PER_MEMS
-      charge += cloud_access_num * charged_request_num * p.LAMBDA_COST_PER_REQ
-      charge += p.OUT_DATA_COST_PER_GB * (charged_out_data / 1000000000)
-    return charge
 
 if __name__ == '__main__':
   model = AISimModel()
